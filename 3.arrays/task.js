60883a45c6c79158a265e56ee16a83baf6f1e37f
Elena Spirina <espirina@list.ru>
2023-01-08 14:34:47 +0400

    DZ array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.age)
+    .reduce((acc, item, index, arr) => {
+      acc += item;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      }
+      return acc;
+    }, 0);
+  return result;
 }
